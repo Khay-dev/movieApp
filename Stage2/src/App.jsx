98b486df064fe20e0ca5c638b6c99a617f63fdb1
Kalu Daniel <krayzhee419@gmail.com>
2023-09-15 16:00:44 +0200

    UPDATE: fixed small problem

--- Stage2/src/App.jsx
+++ Stage2/src/App.jsx
@@ -2,30 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Details from "./Pages/Details";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Nav from "./Component/Nav";
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: Infinity,
-            cacheTime: Infinity,
-        },
-    },
-});
 
 function App() {
     return (
         <>
             <Router>
                 <Nav />
-                <QueryClientProvider client={queryClient}>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/details/:id" element={<Details />} />
-                    </Routes>
-                </QueryClientProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/details/:id" element={<Details />} />
+                </Routes>
             </Router>
         </>
     );
